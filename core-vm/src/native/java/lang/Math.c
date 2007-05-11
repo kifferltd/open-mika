1f6866e221b707d54dfbf63a2902c7972c07efc9
chris <kiffer580@users.noreply.github.com>
2007-05-11 15:19:31 +0000

    Add fast calls for (a)sin, (a)tan, exp, log.

--- core-vm/src/native/java/lang/Math.c
+++ core-vm/src/native/java/lang/Math.c
@@ -38,6 +38,8 @@
 #include "Math.h"
 #include "exception.h"
 
+/* [CG 20070331] None of this seems to be used just now ...
+
 typedef const union {
   unsigned short W[4];
   w_double value;
@@ -49,7 +51,6 @@ typedef const union {
 #define INIT_MATH_CONSTANT(c)              c, 0, 0, 0
 #endif
 
-/*
 **
 **  Representation of a IEEE754 64 bit double value.
 **
@@ -66,31 +67,31 @@ typedef const union {
 **  
 **  and EEEEEEEEEEE = SHORT_3 - VALUE_OF_FFFF_IN_SHORT_3
 **
-*/
 
-#define FRACTION_BITS_IN_SHORT_3                          4  /* _DOFF */
+#define FRACTION_BITS_IN_SHORT_3                          4  /. _DOFF ./
 #define NBITS                                            (48 + FRACTION_BITS_IN_SHORT_3)
 #define _DFRAC                                           ((1 << FRACTION_BITS_IN_SHORT_3)-1)
 #define _DMASK                                           (0x7fff&~_DFRAC)
 #define _DMAX                                            ((1 << (15 - FRACTION_BITS_IN_SHORT_3)) - 1)
 #define _DNAN                                            (0x8000 | _DMAX << FRACTION_BITS_IN_SHORT_3 | 1 << (FRACTION_BITS_IN_SHORT_3 - 1))
-#define VALUE_OF_FFFF_IN_SHORT_3                         0x03fe /* _DBIAS */
+#define VALUE_OF_FFFF_IN_SHORT_3                         0x03fe /. _DBIAS ./
 #define SIGN_BIT_IN_SHORT_3                              0x8000
 #define DIGITS_TO_CONVERT_EACH_PASS                      8
 #define MAX_SIGNIFICANT_DIGITS                           32
 #define MAX_DIGITS_DISPLAYED_DOUBLE			 16
 #define MAX_DIGITS_DISPLAYED_FLOAT			 8 //23 bits in fraction plus hidden 1 --> 24 bits = 16000000
 #define DSIGN(x)                                         (((unsigned short *)&(x))[SHORT_3] & SIGN_BIT_IN_SHORT_3)
+*/
 
 #define HUGE_EXP                                         (int)(_DMAX * 900L / 1000)
 #define SAFE_EXP                                         (_DMAX >> 1)
 #define HUGE_RAD                                         (0x4643d0f18fcc1cb2LL)    /* 3.14e30                  */
 
-static math_constant   HugeVal = {{ INIT_MATH_CONSTANT(_DMAX << FRACTION_BITS_IN_SHORT_3) }};
-static math_constant  Infinite = {{ INIT_MATH_CONSTANT(_DMAX << FRACTION_BITS_IN_SHORT_3) }};
-static math_constant RootOfEps = {{ INIT_MATH_CONSTANT((VALUE_OF_FFFF_IN_SHORT_3 - NBITS / 2) << FRACTION_BITS_IN_SHORT_3) }};
-static math_constant       Big = {{ INIT_MATH_CONSTANT((VALUE_OF_FFFF_IN_SHORT_3 + NBITS / 2) << FRACTION_BITS_IN_SHORT_3) }};
-static math_constant       Nan = {{ INIT_MATH_CONSTANT(_DNAN) }};
+// static math_constant   HugeVal = {{ INIT_MATH_CONSTANT(_DMAX << FRACTION_BITS_IN_SHORT_3) }};
+// static math_constant  Infinite = {{ INIT_MATH_CONSTANT(_DMAX << FRACTION_BITS_IN_SHORT_3) }};
+// static math_constant RootOfEps = {{ INIT_MATH_CONSTANT((VALUE_OF_FFFF_IN_SHORT_3 - NBITS / 2) << FRACTION_BITS_IN_SHORT_3) }};
+// static math_constant       Big = {{ INIT_MATH_CONSTANT((VALUE_OF_FFFF_IN_SHORT_3 + NBITS / 2) << FRACTION_BITS_IN_SHORT_3) }};
+// static math_constant       Nan = {{ INIT_MATH_CONSTANT(_DNAN) }};
 
 static const w_double powers[] = {
   0x4024000000000000LL, /* 1e001 */
@@ -114,8 +115,6 @@ static const w_double factors[] = {
 
 static const w_int numPowers = (sizeof(powers) / sizeof(powers[0]) - 1);
 
-#define MAX_RADIX                     36
-#define MIN_RADIX                      2
 #define MAX_LONG_STRING_SIZE          64
 #define MAX_INT_STRING_SIZE           32
 #define MAX_DOUBLE_STRING_SIZE        64
@@ -148,61 +147,63 @@ static const w_double sincos_s[8] = {
   0x3ff0000000000000LL, /*  1.000000000000000000 */
 };
 
+/*
 static const w_double asincos_p[5] = {
-  0xbfe64bbdb5e61e65LL, /* -0.69674573447350646411e+0 */
-  0x40244e1764ec3927LL, /*  0.10152522233806463645e+2 */
-  0xc043d82ca9a6da9fLL, /* -0.39688862997504877339e+2 */
-  0x404c9aa7360ad48aLL, /*  0.57208227877891731407e+2 */
-  0xc03b5e55a83a0a62LL, /* -0.27368494524164255994e+2 */
+  0xbfe64bbdb5e61e65LL, /. -0.69674573447350646411e+0 ./
+  0x40244e1764ec3927LL, /.  0.10152522233806463645e+2 ./
+  0xc043d82ca9a6da9fLL, /. -0.39688862997504877339e+2 ./
+  0x404c9aa7360ad48aLL, /.  0.57208227877891731407e+2 ./
+  0xc03b5e55a83a0a62LL, /. -0.27368494524164255994e+2 ./
 };
 
 static const w_double asincos_q[6] = {
-  0x3ff0000000000000LL, /*  0.10000000000000000000e+1 */
-  0xc037d2e86ef9861fLL, /* -0.23823859153670238830e+2 */
-  0x4062de7c96591c70LL, /*  0.15095270841030604719e+3 */
-  0xc077ddcefc56a848LL, /* -0.38186303361750149284e+3 */
-  0x407a124f101eb843LL, /*  0.41714430248260412556e+3 */
-  0xc06486c03e2b87ccLL, /* -0.16421096714498560795e+3 */
+  0x3ff0000000000000LL, /.  0.10000000000000000000e+1 ./
+  0xc037d2e86ef9861fLL, /. -0.23823859153670238830e+2 ./
+  0x4062de7c96591c70LL, /.  0.15095270841030604719e+3 ./
+  0xc077ddcefc56a848LL, /. -0.38186303361750149284e+3 ./
+  0x407a124f101eb843LL, /.  0.41714430248260412556e+3 ./
+  0xc06486c03e2b87ccLL, /. -0.16421096714498560795e+3 ./
 };
 
 static const w_double tan_p[3] = {
-  0xbef2bab72ea2c724LL, /* -0.17861707342254426711e-4 */
-  0x3f6c0e82a63baadfLL, /*  0.34248878235890589960e-2 */
-  0xbfc112b5e54d0900LL, /* -0.13338350006421960681e+0 */
+  0xbef2bab72ea2c724LL, /. -0.17861707342254426711e-4 ./
+  0x3f6c0e82a63baadfLL, /.  0.34248878235890589960e-2 ./
+  0xbfc112b5e54d0900LL, /. -0.13338350006421960681e+0 ./
 };
 
 static const w_double tan_q[4] = {
-  0x3ea0b774f07678e9LL, /*  0.49819433993786512270e-6 */
-  0xbf346f6499094841LL, /* -0.31181531907010027307e-3 */
-  0x3f9a479ea17e2159LL, /*  0.25663832289440112864e-1 */
-  0xbfdddeb047fbd9d5LL, /* -0.46671683339755294240e+0 */
+  0x3ea0b774f07678e9LL, /.  0.49819433993786512270e-6 ./
+  0xbf346f6499094841LL, /. -0.31181531907010027307e-3 ./
+  0x3f9a479ea17e2159LL, /.  0.25663832289440112864e-1 ./
+  0xbfdddeb047fbd9d5LL, /. -0.46671683339755294240e+0 ./
 };
 
 static const w_double atan_a[8] = {
-  0x0000000000000000LL, /* 0.00000000000000000000 */
-  0x3fe0c152382d7366LL, /* 0.52359877559829887308 */
-  0x3ff921fb54442d18LL, /* 1.57079632679489661923 */
-  0x3ff0c152382d7366LL, /* 1.04719755119659774615 */
-  0x3ff921fb54442d18LL, /* 1.57079632679489661923 */
-  0x4000c152382d7366LL, /* 2.09439510239319549231 */
-  0x400921fb54442d18LL, /* 3.14159265358979323846 */
-  0x4004f1a6c638d03fLL, /* 2.61799387799149436538 */
+  0x0000000000000000LL, /. 0.00000000000000000000 ./
+  0x3fe0c152382d7366LL, /. 0.52359877559829887308 ./
+  0x3ff921fb54442d18LL, /. 1.57079632679489661923 ./
+  0x3ff0c152382d7366LL, /. 1.04719755119659774615 ./
+  0x3ff921fb54442d18LL, /. 1.57079632679489661923 ./
+  0x4000c152382d7366LL, /. 2.09439510239319549231 ./
+  0x400921fb54442d18LL, /. 3.14159265358979323846 ./
+  0x4004f1a6c638d03fLL, /. 2.61799387799149436538 ./
 };
 
 static const w_double atan_p[4] = {
-  0xbfeacd7ad9b187bdLL, /* -0.83758299368150059274e+0 */
-  0xc020fd3f5c8d6a63LL, /* -0.84946240351320683534e+1 */
-  0xc034817fb9e2bccbLL, /* -0.20505855195861651981e+2 */
-  0xc02b60a651061ce2LL, /* -0.13688768894191926929e+2 */
+  0xbfeacd7ad9b187bdLL, /. -0.83758299368150059274e+0 ./
+  0xc020fd3f5c8d6a63LL, /. -0.84946240351320683534e+1 ./
+  0xc034817fb9e2bccbLL, /. -0.20505855195861651981e+2 ./
+  0xc02b60a651061ce2LL, /. -0.13688768894191926929e+2 ./
 };
 
 static const w_double atan_q[5] = {
-  0x3ff0000000000000LL, /* 0.10000000000000000000e+1 */
-  0x402e0c49e14ac710LL, /* 0.15024001160028576121e+2 */
-  0x404dca0a320da3d7LL, /* 0.59578436142597344465e+2 */
-  0x40558a12040b6da5LL, /* 0.86157349597130242515e+2 */
-  0x4044887cbcc495a9LL, /* 0.41066306682575781263e+2 */
+  0x3ff0000000000000LL, /. 0.10000000000000000000e+1 ./
+  0x402e0c49e14ac710LL, /. 0.15024001160028576121e+2 ./
+  0x404dca0a320da3d7LL, /. 0.59578436142597344465e+2 ./
+  0x40558a12040b6da5LL, /. 0.86157349597130242515e+2 ./
+  0x4044887cbcc495a9LL, /. 0.41066306682575781263e+2 ./
 };
+*/
 
 static const w_double exp_p[3] = {
   0x3f008b442ae6921eLL, /* 0.31555192765684646356e-4 */
@@ -316,6 +317,67 @@ void fast_Math_static_tan(w_frame frame) {
   frame->jstack_top[-2].c = double_x.w[0];
   frame->jstack_top[-1].c = double_x.w[1];
 }
+ 
+void fast_Math_static_asin(w_frame frame) {
+  union {w_double d; w_word w[2];} double_x;
+
+  double_x.w[0] = frame->jstack_top[-2].c;
+  double_x.w[1] = frame->jstack_top[-1].c;
+  if (wfp_float64_is_Infinite(double_x.d)) {
+    double_x.d = D_NAN;
+  }
+  else if (!wfp_float64_is_NaN(double_x.d)) {
+    double_x.d = wfp_float64_asin(double_x.d);
+  }
+  frame->jstack_top[-2].c = double_x.w[0];
+  frame->jstack_top[-1].c = double_x.w[1];
+}
+ 
+void fast_Math_static_atan(w_frame frame) {
+  union {w_double d; w_word w[2];} double_x;
+
+  double_x.w[0] = frame->jstack_top[-2].c;
+  double_x.w[1] = frame->jstack_top[-1].c;
+  if (wfp_float64_is_Infinite(double_x.d)) {
+    double_x.d = wfp_float64_is_negative(double_x.d) ? D_MINUS_HALF_PI : D_HALF_PI;
+  }
+  else if (!wfp_float64_is_NaN(double_x.d)) {
+    double_x.d = wfp_float64_atan(double_x.d);
+  }
+  frame->jstack_top[-2].c = double_x.w[0];
+  frame->jstack_top[-1].c = double_x.w[1];
+}
+
+void fast_Math_static_log(w_frame frame) {
+  union {w_double d; w_word w[2];} double_x;
+
+  double_x.w[0] = frame->jstack_top[-2].c;
+  double_x.w[1] = frame->jstack_top[-1].c;
+  if (wfp_float64_is_Infinite(double_x.d)) {
+    double_x.d = wfp_float64_is_negative(double_x.d) ? D_NAN : double_x.d;
+  }
+  else if (!wfp_float64_is_NaN(double_x.d)) {
+    double_x.d = wfp_float64_log(double_x.d);
+  }
+  frame->jstack_top[-2].c = double_x.w[0];
+  frame->jstack_top[-1].c = double_x.w[1];
+}
+
+void fast_Math_static_exp(w_frame frame) {
+  union {w_double d; w_word w[2];} double_x;
+
+  double_x.w[0] = frame->jstack_top[-2].c;
+  double_x.w[1] = frame->jstack_top[-1].c;
+  if (wfp_float64_is_Infinite(double_x.d)) {
+    double_x.d = wfp_float64_is_negative(double_x.d) ? D_ZERO : double_x.d;
+  }
+  else if (!wfp_float64_is_NaN(double_x.d)) {
+    double_x.d = wfp_float64_exp(double_x.d);
+  }
+  frame->jstack_top[-2].c = double_x.w[0];
+  frame->jstack_top[-1].c = double_x.w[1];
+}
+
 #endif
  
 
