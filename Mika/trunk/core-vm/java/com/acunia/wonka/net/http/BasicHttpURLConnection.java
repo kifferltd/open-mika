51938858acc76eb7f54ef7a2c91d55503c016059
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2007-09-25 12:08:01 +0000

    Licence change.

--- Mika/trunk/core-vm/java/com/acunia/wonka/net/http/BasicHttpURLConnection.java
+++ Mika/trunk/core-vm/java/com/acunia/wonka/net/http/BasicHttpURLConnection.java
@@ -29,7 +29,6 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
 **************************************************************************/
 
-
 package com.acunia.wonka.net.http;
 
 import java.io.*;
