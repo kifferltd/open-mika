91274f192934741df7a4c294435021400a83a7b2
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2007-09-25 12:09:10 +0000

    Licence change.

--- Mika/trunk/core-vm/java/com/acunia/wonka/net/https/Handler.java
+++ Mika/trunk/core-vm/java/com/acunia/wonka/net/https/Handler.java
@@ -26,7 +26,6 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
 **************************************************************************/
 
-
 package com.acunia.wonka.net.https;
 
 import java.net.URLStreamHandler;
