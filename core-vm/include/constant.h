d8ac55157f605e8f160dffc2e6df20fbf36f6465
chris <kiffer580@users.noreply.github.com>
2007-10-14 19:38:28 +0000

    Added a 'thread' parameter to getClassConstant(), and removed the _unsafe variant because it doesn't add any value.

--- core-vm/include/constant.h
+++ core-vm/include/constant.h
@@ -1,3 +1,33 @@
+/**************************************************************************
+* Parts copyright (c) 2001, 2002 by Punch Telematix. All rights reserved. *
+* Parts copyright (c) 2006, 2007 by Chris Gray, /k/ Embedded Java         *
+* Solutions.  All rights reserved.                                        *
+*                                                                         *
+* Redistribution and use in source and binary forms, with or without      *
+* modification, are permitted provided that the following conditions      *
+* are met:                                                                *
+* 1. Redistributions of source code must retain the above copyright       *
+*    notice, this list of conditions and the following disclaimer.        *
+* 2. Redistributions in binary form must reproduce the above copyright    *
+*    notice, this list of conditions and the following disclaimer in the  *
+*    documentation and/or other materials provided with the distribution. *
+* 3. Neither the name of Punch Telematix or of /k/ Embedded Java Solutions*
+*    nor the names of other contributors may be used to endorse or promote*
+*    products derived from this software without specific prior written   *
+*    permission.                                                          *
+*                                                                         *
+* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
+* IN NO EVENT SHALL PUNCH TELEMATIX, /K/ EMBEDDED JAVA SOLUTIONS OR OTHER *
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,     *
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR      *
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  *
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    *
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
+**************************************************************************/
 #ifndef _CONSTANT_H
 #define _CONSTANT_H
 
@@ -256,14 +286,7 @@ static inline w_instance getResolvedStringConstant(w_clazz clazz, w_int i) {
  * Get the value of a CLASS constant, resolving it if need be.
  * The calling thread must GC safe!
  */
-w_clazz getClassConstant(w_clazz clazz, w_int idx);
-
-/**
- * Same as getClassConstant, but the calling thread is GC-unsafe on entry and exit.
- * Since this function may call enterSafeRegion, the thread's GC affairs must
- * already be in order before calling this function.
- */
-w_clazz getClassConstant_unsafe(w_clazz clazz, w_int idx, w_thread thread);
+w_clazz getClassConstant(w_clazz clazz, w_int idx, w_thread thread);
 
 /*
 ** Get the value of a CLASS constant which is known to already be resolved.
