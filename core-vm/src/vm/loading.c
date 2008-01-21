90f8492a5132cdd3faa373a82005b99050b3d372
chris <kiffer580@users.noreply.github.com>
2008-01-21 22:48:56 +0000

    Add "boolean", "byte", etc. to the list of classes which are always to be loaded by the bootstrap class loader, regardless of the initiating class loader.

--- core-vm/src/vm/loading.c
+++ core-vm/src/vm/loading.c
@@ -1,8 +1,8 @@
 /**************************************************************************
 * Parts copyright (c) 2001, 2002, 2003 by Punch Telematix. All rights     *
 * reserved.                                                               *
-* Parts copyright (c) 2004, 2005, 2006, 2007 by Chris Gray, /k/ Embedded  *
-* Java Solutions. All rights reserved.                                    *
+* Parts copyright (c) 2004, 2005, 2006, 2007, 2008 by Chris Gray,         *
+* /k/ Embedded Java Solutions. All rights reserved.                       *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -1351,6 +1351,14 @@ w_boolean namedClassIsSystemClass(w_string name) {
   if (((int)string_length(name) > (ch - name->contents.bytes) + 5 && strncmp(ch, "java.", 5) == 0)
     ||
       ((int)string_length(name) > (ch - name->contents.bytes) + 6 && strncmp(ch, "wonka.", 6) == 0)
+    || strncmp(ch, "boolean", string_length(name)) == 0
+    || strncmp(ch, "byte", string_length(name)) == 0
+    || strncmp(ch, "short", string_length(name)) == 0
+    || strncmp(ch, "int", string_length(name)) == 0
+    || strncmp(ch, "long", string_length(name)) == 0
+    || strncmp(ch, "float", string_length(name)) == 0
+    || strncmp(ch, "double", string_length(name)) == 0
+    || strncmp(ch, "char", string_length(name)) == 0
   ) {
     woempa(1, "'%w' is a system class\n", name);
 
