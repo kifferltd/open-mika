b068ed78f1e07ab2eba2b73500b5aabcdd22a2a1
chris <kiffer580@users.noreply.github.com>
2008-11-20 10:54:01 +0000

    Correct behaviour of JarInputStream by importing the Apache Harmony version of this class.

--- core-vm/java/java/util/jar/JarInputStream.java
+++ core-vm/java/java/util/jar/JarInputStream.java
@@ -1,95 +1,210 @@
-/**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
-*                                                                         *
-* Redistribution and use in source and binary forms, with or without      *
-* modification, are permitted provided that the following conditions      *
-* are met:                                                                *
-* 1. Redistributions of source code must retain the above copyright       *
-*    notice, this list of conditions and the following disclaimer.        *
-* 2. Redistributions in binary form must reproduce the above copyright    *
-*    notice, this list of conditions and the following disclaimer in the  *
-*    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
-*                                                                         *
-* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
-**************************************************************************/
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-/**
- * @version     $Id: JarInputStream.java,v 1.4 2006/04/05 09:20:06 cvs Exp $
+/*
+ * Imported by CG 20081119 based on Apache Harmony ("enhanced") revision 641928.
  */
 
 package java.util.jar;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-/**
- * The JarInputStream is not able to verify signatures ...
- *
- * this option must be added !
- */
 public class JarInputStream extends ZipInputStream {
 
-  private Manifest manifest;
-
-  public JarInputStream (InputStream child) throws IOException {
-    this(child, false);
-  }
-  public JarInputStream (InputStream child, boolean verify) throws IOException {
-    super (child);
-  }
-
-  protected ZipEntry createZipEntry(String ename){
-   	return new JarEntry(ename);   	   	
-  }
-
-  public Manifest getManifest() {
-   	return manifest;
-  }
-
-  private void readManifest() throws IOException {
-      manifest = new Manifest(this);	
-  }
-
-  public ZipEntry getNextEntry() throws IOException {
-    JarEntry je = (JarEntry)super.getNextEntry();
-
-    if (je != null) {
-      if (je.getName().endsWith(JarFile.MANIFEST_NAME)) {
-        readManifest();
-        return getNextEntry();
+  // [CG 20081119] Taken from org.apache.harmony.luni.util.Util
+    public static String toASCIIUpperCase(String s) {
+    int len = s.length();
+    StringBuffer buffer = new StringBuffer(len);
+      for (int i = 0; i < len; i++) {
+        char c = s.charAt(i);
+        if ('a' <= c && c <= 'z') {
+          buffer.append((char) (c - ('a' - 'A')));
+        } else {
+          buffer.append(c);
+        }
       }
-      if (manifest !=  null) {
-  	 je.setAttributes(manifest.getAttributes(je.getName()));
-      }
+      return buffer.toString();
     }
 
-    return je;
-  }
+    private Manifest manifest;
 
-  public JarEntry getNextJarEntry() throws IOException {
-   	return (JarEntry)getNextEntry();
-  }
+    private boolean eos = false;
 
-  public int read(byte [] buf, int offset, int length) throws IOException {
-	  return super.read(buf, offset, length);	
-  }
+    private JarEntry mEntry;
 
+    private JarEntry jarEntry;
 
+    private boolean isMeta;
 
+    // [CG 20081119] We'll come back to this later
+    // private JarVerifier verifier;
 
+    private OutputStream verStream;
+
+    /**
+     * Constructs a new JarInputStream from stream
+     */
+    public JarInputStream(InputStream stream, boolean verify)
+            throws IOException {
+        super(stream);
+    // [CG 20081119] We'll come back to this later
+    //    if (verify) {
+    //        verifier = new JarVerifier("JarInputStream"); //$NON-NLS-1$
+    //    }
+        if ((mEntry = getNextJarEntry()) == null) {
+            return;
+        }
+        String name = toASCIIUpperCase(mEntry.getName());
+        if (name.equals(JarFile.META_DIR)) {
+            mEntry = null; // modifies behavior of getNextJarEntry()
+            closeEntry();
+            mEntry = getNextJarEntry();
+            name = mEntry.getName().toUpperCase();
+        }
+        if (name.equals(JarFile.MANIFEST_NAME)) {
+            mEntry = null;
+    // [CG 20081119] We'll worry about verifying later
+    //        manifest = new Manifest(this, verify);
+            manifest = new Manifest(this);
+
+            closeEntry();
+    // [CG 20081119] We'll come back to this later
+    //        if (verify) {
+    //            verifier.setManifest(manifest);
+    //            if (manifest != null) {
+    //                verifier.mainAttributesEnd = manifest
+    //                        .getMainAttributesEnd();
+    //            }
+    //        }
+
+        } else {
+            Attributes temp = new Attributes(3);
+            temp.map.put("hidden", null); //$NON-NLS-1$
+            mEntry.setAttributes(temp);
+            /*
+             * if not from the first entry, we will not get enough
+             * information,so no verify will be taken out.
+             */
+    // [CG 20081119] We'll come back to this later
+            // verifier = null;
+        }
+    }
+
+    public JarInputStream(InputStream stream) throws IOException {
+        this(stream, true);
+    }
+
+    /**
+     * Returns the Manifest object associated with this JarInputStream or null
+     * if no manifest entry exists.
+     * 
+     * @return java.util.jar.Manifest
+     */
+    public Manifest getManifest() {
+        return manifest;
+    }
+
+    /**
+     * Returns the next JarEntry contained in this stream or null if no more
+     * entries are present.
+     * 
+     * @return java.util.jar.JarEntry
+     * @exception java.io.IOException
+     *                If an error occurs while reading the entry
+     */
+    public JarEntry getNextJarEntry() throws IOException {
+        return (JarEntry) getNextEntry();
+    }
+
+    public int read(byte[] buffer, int offset, int length) throws IOException {
+        if (mEntry != null) {
+            return -1;
+        }
+        int r = super.read(buffer, offset, length);
+        if (verStream != null && !eos) {
+            if (r == -1) {
+                eos = true;
+    // [CG 20081119] We'll come back to this later
+    //            if (verifier != null) {
+    //                if (isMeta) {
+    //                    verifier.addMetaEntry(jarEntry.getName(),
+    //                            ((ByteArrayOutputStream) verStream)
+    //                                    .toByteArray());
+    //                    try {
+    //                        verifier.readCertificates();
+    //                    } catch (SecurityException e) {
+    //                        verifier = null;
+    //                        throw e;
+    //                    }
+    //                } else {
+    //                    ((JarVerifier.VerifierEntry) verStream).verify();
+    //                }
+    //            }
+            } else {
+                verStream.write(buffer, offset, r);
+            }
+        }
+        return r;
+    }
+
+    /**
+     * Returns the next ZipEntry contained in this stream or null if no more
+     * entries are present.
+     * 
+     * @return java.util.zip.ZipEntry
+     * @exception java.io.IOException
+     *                If an error occurs while reading the entry
+     */
+    public ZipEntry getNextEntry() throws IOException {
+        if (mEntry != null) {
+            jarEntry = mEntry;
+            mEntry = null;
+            jarEntry.setAttributes(null);
+        } else {
+            jarEntry = (JarEntry) super.getNextEntry();
+            if (jarEntry == null) {
+                return null;
+            }
+    // [CG 20081119] We'll come back to this later
+    //        if (verifier != null) {
+    //            isMeta = toASCIIUpperCase(jarEntry.getName()).startsWith(
+    //                    JarFile.META_DIR);
+    //            if (isMeta) {
+    //                verStream = new ByteArrayOutputStream();
+    //            } else {
+    //                verStream = verifier.initEntry(jarEntry.getName());
+    //            }
+    //        }
+        }
+        eos = false;
+        return jarEntry;
+    }
+
+    protected ZipEntry createZipEntry(String name) {
+        JarEntry entry = new JarEntry(name);
+        if (manifest != null) {
+            entry.setAttributes(manifest.getAttributes(name));
+        }
+        return entry;
+    }
 }
+
