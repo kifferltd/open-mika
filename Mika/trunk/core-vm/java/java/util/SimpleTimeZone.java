bc1c7175a4e98960cf9e8c4816f4b3bd73cb3a0d
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2008-11-20 18:47:04 +0000

    Modify SimpleTimeZone to give correct offset between (endTime - 1 hour) and (endTime), making use of Apache Harmony and ICU4J code.

--- Mika/trunk/core-vm/java/java/util/SimpleTimeZone.java
+++ Mika/trunk/core-vm/java/java/util/SimpleTimeZone.java
@@ -1,5 +1,7 @@
 /**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
+* Parts copyright (c) 2001 by Punch Telematix. All rights reserved.       *
+* Parts copyright (c) 2008 by Chris Gray, /k/ Embedded Java Solutions.    *
+* All rights reserved.                                                    *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -9,24 +11,23 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
+* 3. Neither the name of Punch Telematix or of /k/ Embedded Java Solutions*
+*    nor the names of other contributors may be used to endorse or promote*
+*    products derived from this software without specific prior written   *
+*    permission.                                                          *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
+* IN NO EVENT SHALL PUNCH TELEMATIX, /K/ EMBEDDED JAVA SOLUTIONS OR OTHER *
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,     *
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR      *
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  *
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    *
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
 **************************************************************************/
-
-
 
 package java.util;
 
@@ -400,7 +401,7 @@ public class SimpleTimeZone extends TimeZone {
   	      	day += ((DOWinM - dayOffweek - 7)% 7);
   	      	int maxDay = monthLength[month]; 	
   	      	if (month == Calendar.FEBRUARY){
-  	       		maxDay += (new GregorianCalendar().isLeapYear(year) ? 1 :0);
+  	       		maxDay += (new GregorianCalendar(this).isLeapYear(year) ? 1 :0);
   	        }
   	        day += (((maxDay - day)/ 7)* 7);
   	        day += 7 * (chDOW+1);
@@ -420,24 +421,24 @@ public class SimpleTimeZone extends TimeZone {
   public boolean inDaylightTime(Date date) {
     GregorianCalendar gc = new GregorianCalendar(this);
     gc.setTime(date);
+    return gc.inDaylightTime();
+/*
     int hour = gc.get(Calendar.HOUR);
     int minute = gc.get(Calendar.MINUTE);
     int second = gc.get(Calendar.SECOND);
     int millisecond = gc.get(Calendar.MILLISECOND);
     return (hour == 0 && minute == 0 && second == 0 && millisecond == 0) ?  inDaylightTime(gc.get(Calendar.ERA), gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), gc.get(Calendar.DATE), gc.get(Calendar.DAY_OF_WEEK), 0) : inDaylightTime(gc.get(Calendar.ERA), gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), gc.get(Calendar.DATE), gc.get(Calendar.DAY_OF_WEEK), (int) (date.getTime() - new GregorianCalendar(gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), gc.get(Calendar.DATE)).getTimeInMillis()));
     // return inDaylightTime(gc.get(Calendar.ERA), gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), gc.get(Calendar.DATE), gc.get(Calendar.DAY_OF_WEEK), ((hour * 60 + minute) * 60 + second) * 1000 + millisecond);
+*/
 
   }
 
+/*
   public int getOffset(int era, int year, int month, int day, int dayOffWeek, int milliseconds) {
    	return rawOffset + (inDaylightTime(era, year, month, day, dayOffWeek, milliseconds) ? dstSavings :0);
   }
-
-/**
-** BAD IMPLEMENTATION
-** couldn't find algorithm to calculate hashCode ...
-** so I made one up using all private fields !
 */
+
   public int hashCode(){
   	int hash = endDay * endDayOfWeek * endMode * endMonth + endTime;
   	hash ^= (startDay * startDayOfWeek * startMode * startMonth + startTime );
@@ -445,7 +446,222 @@ public class SimpleTimeZone extends TimeZone {
   	return hash;
   }
     	
+  /**
+   * Gets the offset from GMT of this SimpleTimeZone for the specified date
+   * and time. The offset includes daylight savings time if the specified date
+   * and time are within the daylight savings time period.
+   * The logic is shamelessly cribbed from ICU4J (icu-project.org). Therefore
+   * here is their copyright and permission notice:
+   * 
+   * <p>COPYRIGHT AND PERMISSION NOTICE
+   * <p>Copyright (c) 1995-2008 International Business Machines Corporation and others 
+   * <p>All rights reserved. 
+   * <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, provided that the above copyright notice(s) and this permission notice appear in all copies of the Software and that both the above copyright notice(s) and this permission notice appear in supporting documentation. 
+   * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
+   * <p>Except as contained in this notice, the name of a copyright holder shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written authorization of the copyright holder.
+   * 
+   * @param era
+   *            the GregorianCalendar era, either GregorianCalendar.BC or
+   *            GregorianCalendar.AD
+   * @param year
+   *            the year
+   * @param month
+   *            the Calendar month
+   * @param day
+   *            the day of the month
+   * @param dayOfWeek
+   *            the Calendar day of the week
+   * @param time
+   *            the time of day in milliseconds
+   * @return the offset from GMT in milliseconds
+   */
+  public int getOffset(int era, int year, int month, int day, int dayOfWeek, int time) {
+    if (era != GregorianCalendar.BC && era != GregorianCalendar.AD) {
+      throw new IllegalArgumentException("bad era: " + era);
+    }
+    if (month < Calendar.JANUARY || month > Calendar.DECEMBER) {
+      throw new IllegalArgumentException("bad month: " + month);
+    }
+    if (dayOfWeek < Calendar.SUNDAY || dayOfWeek > Calendar.SATURDAY) {
+      throw new IllegalArgumentException("bad dayOfWeek: " + dayOfWeek);
+    }
+    if (time < 0 || time >= 24 * 3600000) {
+      throw new IllegalArgumentException("bad offset: " + time);
+    }
+//System.out.println("year = " + year + " month = " + month + " day = " + day + " dOW = " + dayOfWeek + " time = " + time);
+    boolean leap = (year % 4 == 0);
+    if (leap && year > 1582) {
+      leap = (year % 100 != 0 || year % 400 == 0);
+    }
+//System.out.println(year + " is" + (leap ? " " : " not ") + "a leap year");
 
+    if (month != Calendar.FEBRUARY || day != 29 || !leap) {
+      if (day <= 0 || day > GregorianCalendar.DaysInMonth[month]) {
+        throw new IllegalArgumentException("bad day: " + day);
+      }
+    }
+
+    int monlen = monthLength[month];
+    if (leap && month == Calendar.FEBRUARY) {
+      ++monlen;
+    }
+    int prevmonlen = monthLength[(month + 11) % 12];
+    if (leap && month == Calendar.MARCH) {
+      ++prevmonlen;
+    }
+//System.out.println("month " + month + " length = " + monlen + ", previous = " + prevmonlen);
+    int offset = rawOffset;
+
+    if (useDaylight && year >= startYear && era == GregorianCalendar.AD) {
+      boolean downUnder = startMonth > endMonth;
+//System.out.println((downUnder ? "nor" : "sou") + "thern hemisphere");
+
+      while (time > 24 * 3600000) {
+        time -= 24 * 3600000;
+        ++day;
+        dayOfWeek = (dayOfWeek % 7) + 1;
+        if (day > monlen) {
+          ++month;
+          day = 1;
+        }
+      }
+      while (time < 0) {
+        time += 24 * 3600000;
+        --day;
+        dayOfWeek = ((dayOfWeek + 5) % 7) + 1;
+        if (day < 1) {
+          --month;
+          day = prevmonlen;
+        }
+      }
+      // At this point 'month' could  be out of range but this does no harm.
+//System.out.println("corrected year = " + year + " month = " + month + " day = " + day + " dOW = " + dayOfWeek + " time = " + time);
+
+//System.out.println("startMonth = " + startMonth + " startDay = " + startDay);
+      int startCompare = 0;
+      if (month < startMonth) {
+        startCompare = -1;
+      }
+      else if (month > startMonth) {
+        startCompare = 1;
+      }
+      else {
+        int startDayOfMonth = 0;
+        if (startDay > monlen) {
+          startDay = monlen;
+        }
+
+        switch (startMode) {
+        case DOM_MODE:
+          startDayOfMonth = startDay;
+          break;
+        case DOW_IN_MONTH_MODE:
+          if (startDay > 0)
+            startDayOfMonth = 1 + (startDay - 1) * 7 +
+                (7 + startDayOfWeek - (dayOfWeek - day + 1)) % 7;
+          else {
+            startDayOfMonth = monlen + (startDay + 1) * 7 -
+                (7 + (dayOfWeek + monlen - day) - startDayOfWeek) % 7;
+          }
+          break;
+        case DOW_GE_DOM_MODE:
+          startDayOfMonth = startDay +
+            (49 + startDayOfWeek - startDay - dayOfWeek + day) % 7;
+          break;
+        case DOW_LE_DOM_MODE:
+          startDayOfMonth = startDay -
+            (49 - startDayOfWeek + startDay + dayOfWeek - day) % 7;
+          break;
+        }
+
+//System.out.println("startDayOfMonth = " + startDayOfMonth + " startTime = " + startTime);
+        if (day < startDayOfMonth) {
+          startCompare = -1;
+        }
+        else if (day > startDayOfMonth) {
+          startCompare = 1;
+        }
+        else {
+          if (time < startTime){
+            startCompare = -1;
+          }
+          else if (time > startTime){
+            startCompare = 1;
+          }
+        }
+      }
+//System.out.println("startCompare = " + startCompare);
+
+//System.out.println("endMonth = " + endMonth + " endDay = " + endDay);
+      int endCompare = 0;
+      if (downUnder != (startCompare >= 0)) {
+        if (month < endMonth) {
+          endCompare = -1;
+        }
+        else if (month > endMonth) {
+          endCompare = 1;
+        }
+        else {
+          int endDayOfMonth = 0;
+          if (endDay > monlen) {
+            endDay = monlen;
+          }
+
+          switch (endMode) {
+          case DOM_MODE:
+            endDayOfMonth = endDay;
+            break;
+          case DOW_IN_MONTH_MODE:
+            if (endDay > 0)
+              endDayOfMonth = 1 + (endDay - 1) * 7 +
+                  (7 + endDayOfWeek - (dayOfWeek - day + 1)) % 7;
+            else {
+              endDayOfMonth = monlen + (endDay + 1) * 7 -
+                  (7 + (dayOfWeek + monlen - day) - endDayOfWeek) % 7;
+            }
+            break;
+          case DOW_GE_DOM_MODE:
+            endDayOfMonth = endDay +
+              (49 + endDayOfWeek - endDay - dayOfWeek + day) % 7;
+            break;
+          case DOW_LE_DOM_MODE:
+            endDayOfMonth = endDay -
+              (49 - endDayOfWeek + endDay + dayOfWeek - day) % 7;
+            break;
+          }
+
+//System.out.println("endDayOfMonth = " + endDayOfMonth + " endTime = " + endTime);
+          if (day < endDayOfMonth) {
+            endCompare = -1;
+          }
+          else if (day > endDayOfMonth) {
+            endCompare = 1;
+          }
+          else {
+            time += dstSavings;
+            // Following ICU4J we should re-normalize here in case adding
+            // DST has pushed us into tomorrow. But that seems pretty far-
+            // fetched so we don't bother.
+            if (time < endTime){
+              endCompare = -1;
+            }
+            else if (time > endTime){
+              endCompare = 1;
+            }
+          }
+        }
+      }
+//System.out.println("endCompare = " + endCompare);
+
+      if ((!downUnder && (startCompare >= 0 && endCompare < 0)) ||
+            (downUnder && (startCompare >= 0 || endCompare < 0))) {
+//System.out.println("applying DST");
+        offset += dstSavings;
+      }
+    }
+
+    return offset;
+  }
 
 }
 
