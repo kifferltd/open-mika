5fb2870e6321983d21c9a6cf4e06f252f13f2609
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2008-11-22 13:04:16 +0000

    When looking up the display names, follow aliases: i.e. if the key turns out to be an alias, assign key <- value and repeat the search. So if for example the key is Europe/Brussels we will find that this an alias for ECT and fetch the display names for ECT.

--- Mika/trunk/core-vm/java/wonka/resource/TimeZoneDisplayNameResourceBundle.java
+++ Mika/trunk/core-vm/java/wonka/resource/TimeZoneDisplayNameResourceBundle.java
@@ -1,5 +1,7 @@
 /**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
+* Parts copyright (c) 2001 by Punch Telematix. All rights reserved.       *
+* Parts copyright (c) 2008 by Chris Gray, /k/ Embedded Java Solutions.    *
+* All rights reserved.                                                    *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -9,21 +11,22 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
+* 3. Neither the name of Punch Telematix or of /k/ Embedded Java Solutions*
+*    nor the names of other contributors may be used to endorse or promote*
+*    products derived from this software without specific prior written   *
+*    permission.                                                          *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
+* IN NO EVENT SHALL PUNCH TELEMATIX, /K/ EMBEDDED JAVA SOLUTIONS OR OTHER *
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,     *
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR      *
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  *
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    *
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
 **************************************************************************/
 
 package wonka.resource;
@@ -32,6 +35,7 @@ import java.util.ResourceBundle;
 import java.util.Hashtable;
 import java.util.Enumeration;
 import java.util.MissingResourceException;
+import java.util.TimeZone;
 
 public class TimeZoneDisplayNameResourceBundle extends ResourceBundle {
 
@@ -44,10 +48,14 @@ public class TimeZoneDisplayNameResourceBundle extends ResourceBundle {
   //required implementation of abstract methods of ResourceBundle
   protected Object handleGetObject(String key) throws MissingResourceException {
    	Object o = timeZoneNames.get(key);
+        while (o instanceof String) {
+          // follow alias
+          o = timeZoneNames.get(o);
+        }
    	if (o != null) {
    		return o;
    	}
-   	throw new MissingResourceException("Oops, resource not found","TimeZoneDisplayResourceBundle","key");
+   	throw new MissingResourceException("Oops, resource '" + key + "' not found","TimeZoneDisplayResourceBundle","key");
   }
 
   public Enumeration getKeys() {
