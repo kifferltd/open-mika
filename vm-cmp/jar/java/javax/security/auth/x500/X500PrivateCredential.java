8d9ce5d160d6b5f68ea6c5edf30e878b62febff6
chris <kiffer580@users.noreply.github.com>
2009-03-25 07:39:48 +0000

    Replaced Gnu jaxax.security by Harmony code.

--- vm-cmp/jar/java/javax/security/auth/x500/X500PrivateCredential.java
+++ vm-cmp/jar/java/javax/security/auth/x500/X500PrivateCredential.java
@@ -1,149 +1,77 @@
-/* X500PrivateCredential.java -- certificate and private key pair.
-   Copyright (C) 2003, 2004  Free Software Foundation, Inc.
-
-This file is part of GNU Classpath.
-
-GNU Classpath is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-GNU Classpath is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GNU Classpath; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301 USA.
-
-Linking this library statically or dynamically with other modules is
-making a combined work based on this library.  Thus, the terms and
-conditions of the GNU General Public License cover the whole
-combination.
-
-As a special exception, the copyright holders of this library give you
-permission to link this library with independent modules to produce an
-executable, regardless of the license terms of these independent
-modules, and to copy and distribute the resulting executable under
-terms of your choice, provided that you also meet, for each linked
-independent module, the terms and conditions of the license of that
-module.  An independent module is a module which is not derived from
-or based on this library.  If you modify this library, you may extend
-this exception to your version of the library, but you are not
-obligated to do so.  If you do not wish to do so, delete this
-exception statement from your version. */
-
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 package javax.security.auth.x500;
 
-import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-
+import java.security.PrivateKey;
 import javax.security.auth.Destroyable;
 
-/**
- * A pairing of a {@link X509Certificate} and its corresponding {@link
- * PrivateKey}, with an optional keystore alias.
- */
-public final class X500PrivateCredential implements Destroyable
-{
+public final class X500PrivateCredential implements Destroyable {
 
-  // Fields.
-  // -------------------------------------------------------------------------
+    //X509 certificate
+    private X509Certificate cert;
 
-  private PrivateKey key;
-  private X509Certificate certificate;
-  private String alias;
+    //Private key
+    private PrivateKey key;
 
-  // Constructors.
-  // -------------------------------------------------------------------------
+    //Alias
+    private String alias;
 
-  /**
-   * Creates a new private credential with no associated keystore alias.
-   *
-   * @param certificate The X.509 certificate.
-   * @param key The private key.
-   * @throws IllegalArgumentException If either parameter is null.
-   */
-  public X500PrivateCredential (X509Certificate certificate, PrivateKey key)
-  {
-    if (certificate == null || key == null)
-      throw new IllegalArgumentException();
-    this.certificate = certificate;
-    this.key = key;
-  }
+    public X500PrivateCredential(X509Certificate cert, PrivateKey key) {
+        super();
+        if (cert == null) {
+            throw new IllegalArgumentException("X509 certificate is null");
+        }
+        if (key == null) {
+            throw new IllegalArgumentException("Private key is null");
+        }
+        this.cert = cert;
+        this.key = key;
+    }
 
-  /**
-   * Creates a new private credential with a keystore alias.
-   *
-   * @param certificate The X.509 certificate.
-   * @param key The private key.
-   * @param alias The keystore alias for this credential.
-   * @throws IllegalArgumentException If any parameter is null.
-   */
-  public X500PrivateCredential (X509Certificate certificate, PrivateKey key,
-                                String alias)
-  {
-    this (certificate, key);
-    if (alias == null)
-      throw new IllegalArgumentException();
-    this.alias = alias;
-  }
+    public X500PrivateCredential(X509Certificate cert, PrivateKey key, String alias) {
+        this(cert, key);
+        if (alias == null) {
+            throw new IllegalArgumentException("Alias is null");
+        }
+        this.alias = alias;
+    }
 
-  // Instance methods.
-  // -------------------------------------------------------------------------
+    public X509Certificate getCertificate() {
+        return cert;
+    }
 
-  /**
-   * Returns the certificate of this credential.
-   *
-   * @return The certificate of this credential.
-   */
-  public X509Certificate getCertificate()
-  {
-    return certificate;
-  }
+    public PrivateKey getPrivateKey() {
+        return key;
+    }
 
-  /**
-   * Returns the private key of this credential.
-   *
-   * @return The private key of this credential.
-   */
-  public PrivateKey getPrivateKey()
-  {
-    return key;
-  }
+    public String getAlias() {
+        return alias;
+    }
 
-  /**
-   * Returns the keystore alias of this credential, or null if not present.
-   *
-   * @return The keystore alias, or null.
-   */
-  public String getAlias()
-  {
-    return alias;
-  }
+    public void destroy() {
+        cert = null;
+        key = null;
+        alias = null;
+    }
 
-  /**
-   * Destroy the sensitive data of this credential, setting the certificate,
-   * private key, and keystore alias to null.
-   */
-  public void destroy()
-  {
-    certificate = null;
-    key = null;
-    alias = null;
-  }
-
-  /**
-   * Tells whether or not this credential has been destroyed, and that
-   * the certificate and private key fields are null.
-   *
-   * @return True if this object has been destroyed.
-   */
-  public boolean isDestroyed()
-  {
-    return certificate == null && key == null;
-  }
+    public boolean isDestroyed() {
+        return (cert == null && key == null && alias == null);
+    }
 }
+
