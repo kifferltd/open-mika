e6633d4cb823df77c538e715cf4bb402d6a2fad4
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2009-06-28 09:07:59 +0000

    Removing another old, leaky tag.

--- Mika/tags/PKG-adhoco-2/sample/apps/java/wonka/test/SystemPropertyTest.java
+++
@@ -1,7 +0,0 @@
-package wonka.test;
-
-public class SystemPropertyTest {
-  public static void main(String[] args) {
-    System.getProperties().list(System.out);
-  }
-}
--- Mika/tags/PKG-adhoco-2/vm-cmp/awt/rudolph/hal/hostos/netbsd/include/sound.h
+++
@@ -1,5 +0,0 @@
-
-#include "wonka.h"
- 
-w_void play_au(w_ubyte *file_data);
-
