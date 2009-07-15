9f4767c2c036c4bb330172a77fcfd7d5e74ab5b9
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2009-07-15 15:13:16 +0000

    Remove mechanism to stop time going backwards, it can cause problems if ntpd is being used.

--- Mika/trunk/core-vm/hal/hostos/linux/src/systemtime.c
+++ Mika/trunk/core-vm/hal/hostos/linux/src/systemtime.c
@@ -30,44 +30,14 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-/*
- * Result of the previous call to getNativeSystemTime. We don't allow time to go backwards!
- */
-static volatile w_long previous_time;
-/*
- * Unless, that is, the update_counter goes backwards too.
- * To understand why, imagine that a clock interrupt occurs between the call to gettimeofday()
- * and the succeeding 'if' statement. Then a thread switch might occur, and another thread 
- * might enter this routine and call gettimeofday(). This other thread would presumably get
- * a timeval later than ours, and also later than that stored in previous_time; so it will
- * go ahead and update previous_time and return normally. Now when our thread reaches its 'if'
- * statement it will see the new value of previous_time and get terribly upset. By reading the
- * time_update_counter before calling gettimeofday(), and updating it after updating 
- * previous_time, we are able to detect and ignore this apparent anomaly.
- */
-static volatile w_int time_update_counter;
-
-
 w_long getNativeSystemTime(void) {
   w_long result;
-  w_long counter;
   struct timeval now;
   
-  counter = time_update_counter;
-//  [CG 20040331] You can put this voodoo in, but it slows you down ...
-//  usleep(0);
   gettimeofday(&now, NULL);
   result = now.tv_sec;
   result *= 1000;
   result += (now.tv_usec + 500) / 1000;
-  if (result < previous_time && counter > time_update_counter) {
-//    printf ("getNativeSystemTime(): going backwards! Was %lld, now %lld\n", previous_time, result);
-    result = previous_time;
-  }
-  else {
-    previous_time = result;
-    time_update_counter = counter + 1;
-  }
 
   return result;
 }
