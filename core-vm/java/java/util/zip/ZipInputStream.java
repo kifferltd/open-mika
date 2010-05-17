52c0abbbf21aeda8ece29f3a110e666815767d05
chris <kiffer580@users.noreply.github.com>
2010-05-17 09:19:41 +0000

    Correctly handle archives in which the local file header is not quite the same as the one in the central directory (different length of "extra" string e.g.); plus some improvements in error reporting.

--- core-vm/java/java/util/zip/ZipInputStream.java
+++ core-vm/java/java/util/zip/ZipInputStream.java
@@ -1,5 +1,7 @@
 /**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
+* Parts copyright (c) 2001 by Punch Telematix. All rights reserved.       *
+* Parts copyright (c) 2010 by Chris Gray, /k/ Embedded Java Solutions.    *
+* All rights reserved.                                                    *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -9,27 +11,24 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
+* 3. Neither the name of Punch Telematix or of /k/ Embedded Java Solutions*
+*    nor the names of other contributors may be used to endorse or promote*
+*    products derived from this software without specific prior written   *
+*    permission.                                                          *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
+* IN NO EVENT SHALL PUNCH TELEMATIX, /K/ EMBEDDED JAVA SOLUTIONS OR OTHER *
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,     *
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR      *
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  *
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    *
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
 **************************************************************************/
 
-
-/**
- * $Id: ZipInputStream.java,v 1.6 2006/03/27 08:19:06 cvs Exp $
- */
 package java.util.zip;
 
 import java.io.InputStream;
@@ -602,7 +601,14 @@ public class ZipInputStream extends InflaterInputStream implements ZipConstants 
       throw new ZipException("invalid header data");
     }
     pos += 10;
-    ze.pointer = ZipFile.bytesToLong(b, pos) + len + hlp + com + 30;
+    int localFHoffs = (int)ZipFile.bytesToLong(b, pos);
+    int localFHlen = (0x0ff & (char) b[localFHoffs + 26]) + (0x0ff & (char) b[localFHoffs + 27]) * 256;
+    // TODO: this should be the same as in global FH I guess
+    int localFHxlen = (0x0ff & (char) b[localFHoffs + 28]) + (0x0ff & (char) b[localFHoffs + 29]) * 256;
+    // apparently this can be different to the global FH
+
+    // WAS: ze.pointer = ZipFile.bytesToLong(b, pos) + len + hlp + com + 30;
+    ze.pointer = localFHoffs + localFHlen + localFHxlen + 30;
     pos += 4 + len;
     if (hlp > 0) {
       byte[] extra = new byte[hlp];
@@ -668,7 +674,9 @@ public class ZipInputStream extends InflaterInputStream implements ZipConstants 
       }
       return res;
     } catch (DataFormatException e) {
-      throw new ZipException(e.toString());
+      ZipException zexc = new ZipException();
+      zexc.initCause(e);
+      throw zexc;
     }  
   }
 /*
