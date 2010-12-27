e7e6faa111a090c5b013118441901ea0e9dde9dc
chris <kiffer580@users.noreply.github.com>
2010-12-27 19:30:18 +0000

    Yet more java.util stuff.

--- core-vm/java/java/util/AbstractSequentialList.java
+++ core-vm/java/java/util/AbstractSequentialList.java
@@ -1,108 +1,88 @@
-/**************************************************************************
-* Parts copyright (c) 2001 by Punch Telematix. All rights reserved.       *
-* Parts copyright (c) 2005 by /k/ Embedded Java Solutions.                *
-* All rights reserved.                                                    *
-*                                                                         *
-* Redistribution and use in source and binary forms, with or without      *
-* modification, are permitted provided that the following conditions      *
-* are met:                                                                *
-* 1. Redistributions of source code must retain the above copyright       *
-*    notice, this list of conditions and the following disclaimer.        *
-* 2. Redistributions in binary form must reproduce the above copyright    *
-*    notice, this list of conditions and the following disclaimer in the  *
-*    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix or of /k/ Embedded Java Solutions*
-*    nor the names of other contributors may be used to endorse or promote*
-*    products derived from this software without specific prior written   *
-*    permission.                                                          *
-*                                                                         *
-* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX, /K/ EMBEDDED JAVA SOLUTIONS OR OTHER *
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,     *
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR      *
-* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  *
-* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    *
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
-**************************************************************************/
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * Imported by CG 20101225 based on Apache Harmony ("enhanced") revision 929253.
+ */
 
 package java.util;
 
+/**
+ * AbstractSequentialList is an abstract implementation of the List interface.
+ * This implementation does not support adding. A subclass must implement the
+ * abstract method listIterator().
+ * 
+ * @since 1.2
+ */
 public abstract class AbstractSequentialList extends AbstractList {
 
-// All iterators of this class are fail-fast --> if you override a method of this class
-//
+    /**
+     * Constructs a new instance of this AbstractSequentialList.
+     */
+    protected AbstractSequentialList() {
+        super();
+    }
 
-  protected AbstractSequentialList() {
-  }
+    public void add(int location, Object object) {
+        listIterator(location).add(object);
+    }
 
-  public abstract ListIterator listIterator(int index);
+    public boolean addAll(int location, Collection collection) {
+        ListIterator it = listIterator(location);
+        Iterator colIt = collection.iterator();
+        int next = it.nextIndex();
+        while (colIt.hasNext()) {
+            it.add(colIt.next());
+        }
+        return next != it.nextIndex();
+    }
 
-  public Object get(int index) {
-  	if (index >= size() || index < 0) {
-  		throw new IndexOutOfBoundsException();
-  	}
-  	ListIterator lit = listIterator(index);
-  	//System.out.println("get in abstractSeq "+index);
-  	//System.out.println("returning from get in abstractSeq "+index);
-  	return lit.next();
-  }
+    public Object get(int location) {
+        try {
+            return listIterator(location).next();
+        } catch (NoSuchElementException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
 
-  public Object set(int index, Object element)
-    throws UnsupportedOperationException, ClassCastException, IllegalArgumentException
-   {
-  	if (index >= size() || index < 0) {
-  		throw new IndexOutOfBoundsException();
-  	}
-  	ListIterator lit = listIterator(index);
-  	Object o = lit.next();
-  	lit.set(element);
-  	return o;
-  }
+    public Iterator iterator() {
+        return listIterator(0);
+    }
 
-  public Object remove(int index) throws UnsupportedOperationException {
-  	if (index >= size() || index < 0) {
-  		throw new IndexOutOfBoundsException();
-  	}
-  	ListIterator lit = listIterator(index);
-  	Object o = lit.next();
-  	lit.remove();
-  	return o;
-  }
+    public abstract ListIterator listIterator(int location);
 
-  public void add(int index, Object element)
-    throws UnsupportedOperationException, ClassCastException, IllegalArgumentException   {
-  	if (index > size() || index < 0) {
-  		throw new IndexOutOfBoundsException();
-  	}
-  	ListIterator lit = listIterator(index);
-  	lit.add(element);
-  }
+    public Object remove(int location) {
+        try {
+            ListIterator it = listIterator(location);
+            Object result = it.next();
+            it.remove();
+            return result;
+        } catch (NoSuchElementException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
 
-  public boolean addAll(Collection c) {
-   	return addAll(size(),c);
-  }
-
-  public boolean addAll(int index, Collection c) {
-  	if (index > size() || index < 0) {
-  		throw new IndexOutOfBoundsException();
-  	}
-  	if (c.size() == 0) {
-  		return false;
-  	}
-  	Iterator it = c.iterator();
-  	ListIterator lit = listIterator(index);
-  	while (it.hasNext()) {
-  		lit.add(it.next());
-  	}
-  	return true;
-  }
-
-  public Iterator iterator() {
-    return listIterator(0);
-  }
+    public Object set(int location, Object object) {
+        ListIterator it = listIterator(location);
+        if (!it.hasNext()) {
+            throw new IndexOutOfBoundsException();
+        }
+        Object result = it.next();
+        it.set(object);
+        return result;
+    }
 }
-
