4de8bb6a9fa0176e9e11ec464411be4a74fe6ab7
chris <chris@b92f389e-201f-0410-be35-d4ed221077a5>
2014-03-23 18:00:42 +0000

    Fix memory leak in java.util.Timer.

--- Mika/trunk/core-vm/java/java/util/Timer.java
+++ Mika/trunk/core-vm/java/java/util/Timer.java
@@ -1,5 +1,6 @@
 /**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
+* Copyright (c) 2009, 2011, 2014 by Chris Gray, KIFFER Ltd.               *
+* All rights reserved.                                                    *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -9,33 +10,86 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
+* 3. Neither the name of KIFFER Ltd nor the names of other contributors   *
+*    may be used to endorse or promote products derived from this         *
+*    software without specific prior written permission.                  *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
+* IN NO EVENT SHALL KIFFER LTD OR OTHER CONTRIBUTORS BE LIABLE FOR ANY    *
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      *
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS *
+* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   *
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,     *
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING   *
+* IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      *
+* POSSIBILITY OF SUCH DAMAGE.                                             *
 **************************************************************************/
-
-
-/*
-** $Id: Timer.java,v 1.1.1.1 2004/07/12 14:07:47 cvs Exp $
-*/
 
 package java.util;
 
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.PhantomReference;
+
 public class Timer {
 
-  private TimerThread thread;
+  /**
+   * We use phantom references to track instances of 
+   * java.util.Timer which have become unreachable.
+   * When the PhantomReference is enqueued we interrupt
+   * the TimerThread, which will see that its associated
+   * Timer is unreachable and so will terminate.  Then
+   * GC can do its work and reclaim the whole caboodle.
+   * <p>We store the phantom references in a HashSet so
+   * that thsy themselves do not get garbage-collected.
+   */
+  private static Set phantomReferences = new HashSet();
+
+  private static ReferenceQueue queue = new ReferenceQueue();
+
+  private static class TimerPhantomReference extends PhantomReference {
+    private TimerThread timerThread;
+
+    private TimerPhantomReference(Timer timer, ReferenceQueue queue, TimerThread timerThread) {
+      super(timer, queue);
+      this.timerThread = timerThread;
+    }
+
+    private void cleanup() {
+      timerThread.interrupt();
+    }
+  }
+ 
+  static {
+ 
+    /**
+     * This is the daemon thread which collects phantom 
+     * references from the queue and interrupts the 
+     * corresponding TimerThread.
+     */
+    Runnable referenceGleaner = new Runnable() {
+      public void run() {
+        while (true) {
+          try {
+            TimerPhantomReference tpr = (TimerPhantomReference)queue.remove();
+            tpr.cleanup();
+            synchronized (phantomReferences) {
+              phantomReferences.remove(tpr);
+            }
+          } catch (Exception ex) {
+            // ignore
+          }
+        }
+      }
+    };
+
+    Thread gleanerThread = new Thread(referenceGleaner, "java.util.Timer clean-up thread");
+    gleanerThread.setDaemon(true);
+    gleanerThread.start();
+  }
+
+  private final TimerThread thread;
 
   public Timer(){
     this(false);
@@ -43,6 +97,9 @@ public class Timer {
 
   public Timer(boolean daemon){
     thread = new TimerThread(this, daemon);
+    synchronized (phantomReferences) {
+      phantomReferences.add(new TimerPhantomReference(this, queue, thread));
+    }
   }
 
   public void cancel(){
