8248e75a6c90132a28441fac52a5de4f263be77b
kiffer580 <chris.gray@kiffer.ltd.uk>
2016-03-19 12:36:56 +0000

    Ensure all jdwp responses correctly tag type as class/interface/array.

--- vm-ext/jpda/jdwp/src/cmd-vm.c
+++ vm-ext/jpda/jdwp/src/cmd-vm.c
@@ -1,7 +1,6 @@
 /**************************************************************************
-* Parts copyright (c) 2001 by Punch Telematix. All rights reserved.       *
-* Parts copyright (c) 2004, 2006 by Chris Gray, /k/ Embedded Java         *
-* Solutions. All rights reserved.                                         *
+* Copyright (c) 2004, 2006, 2016 by Chris Gray, KIFFER Ltd.               *
+* All rights reserved.                                                    *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -11,22 +10,21 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix or of /k/ Embedded Java Solutions*
-*    nor the names of other contributors may be used to endorse or promote*
-*    products derived from this software without specific prior written   *
-*    permission.                                                          *
+* 3. Neither the name of KIFFER Ltd nor the names of other contributors   *
+*    may be used to endorse or promote products derived from this         *
+*    software without specific prior written permission.                  *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX, /K/ EMBEDDED JAVA SOLUTIONS OR OTHER *
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,   *
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,     *
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR      *
-* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  *
-* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    *
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            *
+* IN NO EVENT SHALL KIFFER LTD OR OTHER CONTRIBUTORS BE LIABLE FOR ANY    *
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      *
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE       *
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS           *
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,            *
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,       *
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                      *
 **************************************************************************/
 
 #include "clazz.h"
@@ -227,8 +225,7 @@ static void jdwp_vm_classes_by_sig(jdwp_command_packet cmd) {
 
     while ((clazz = getFifo(clazz_fifo))) {
       woempa(1, "  %K\n", clazz);
-      jdwp_put_u1(&reply_grobag, isSet(clazz->flags, ACC_INTERFACE) ? jdwp_tt_interface : jdwp_tt_class);
-      jdwp_put_clazz(&reply_grobag, clazz);
+      jdwp_put_tagged_type(&reply_grobag, clazz);
       jdwp_put_u4(&reply_grobag, clazz2status(clazz));
     }
     releaseFifo(clazz_fifo);
@@ -278,8 +275,7 @@ static void jdwp_vm_classes(jdwp_command_packet cmd) {
     jdwp_put_u4(&reply_grobag, occupancyOfFifo(clazz_fifo));
     woempa(7, "Reading clazz_fifo\n");
     while ((clazz = getFifo(clazz_fifo))) {
-      jdwp_put_u1(&reply_grobag, isSet(clazz->flags, ACC_INTERFACE) ? jdwp_tt_interface : jdwp_tt_class);
-      jdwp_put_clazz(&reply_grobag, clazz);
+      jdwp_put_tagged_type(&reply_grobag, clazz);
       sig_string = clazz2desc(clazz);
       woempa(1, "  %K descriptor %w\n", clazz, sig_string);
       sig = jdwp_string2UTF8(sig_string, &length);
