791dd33887a727df2728be7d81f443117dd80445
kiffer580 <chris.gray@kiffer.ltd.uk>
2021-10-04 11:50:25 +0200

    Add FREERTOS_IO_INCLUDE_DIR.

---
+++ WonkaInfo.mk
@@ -0,0 +1,77 @@
+export WONKA_INFO
+
+ifdef CCLASSPATH
+  WONKA_INFO += runtime classpath is $(CCLASSPATH);
+endif
+
+ifeq ($(FILESYSTEM), vfs)
+  WONKA_INFO += using own virtual filesystem\;
+endif
+ 
+ifeq ($(FILESYSTEM), native)
+  WONKA_INFO += using host OS filesystem, with virtual root at $(FSROOT)\;
+endif
+
+ifeq ($(JAVA5_SUPPORT), true)
+  WONKA_INFO += with Java5 support\;
+endif
+
+ifeq ($(JAVA5_SUPPORT), false)
+  WONKA_INFO += no Java5 support\;
+endif
+
+ifeq ($(JDWP), true)
+  WONKA_INFO += with JDWP enabled\;
+endif
+
+ifeq ($(JDWP), false)
+  WONKA_INFO += no JDWP\;
+endif
+
+ifeq ($(USE_LIBFFI), true)
+  WONKA_INFO += using libffi to call native code\;
+endif
+
+ifeq ($(USE_LIBFFI), false)
+  WONKA_INFO += using own hacks to call native code\;
+endif
+
+ifeq ($(BYTECODE_VERIFIER), true)
+  WONKA_INFO += bytecode verification is enabled\;
+endif
+
+ifeq ($(BYTECODE_VERIFIER), false)
+  WONKA_INFO += bytecode verification is disabled\;
+endif
+
+ifeq ($(NETWORK), none)
+  WONKA_INFO += no network\;
+endif
+
+ifeq ($(NETWORK), native)
+  WONKA_INFO += using the host OS network facilities\;
+endif
+
+ifeq ($(SECURITY), java2)
+    WONKA_INFO += fine-grained (Java2) security\;
+endif
+ifeq ($(SECURITY), none)
+    WONKA_INFO += no security\;
+endif
+
+ifeq ($(FLOATING_POINT), native)
+  WONKA_INFO += using native floating-point\;
+endif
+
+ifeq ($(FLOATING_POINT), hauser)
+  WONKA_INFO += using own floating-point after John Hauser\;
+endif
+
+ifeq ($(MATH), native)
+  WONKA_INFO += using native math functions\;
+endif
+
+ifeq ($(MATH), java)
+  WONKA_INFO += using all-java math functions\;
+endif
+
