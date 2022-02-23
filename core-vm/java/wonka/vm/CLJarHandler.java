32f72d394fd2f416409e811d3a3043c1f690749e
kiffer580 <chris.gray@kiffer.ltd.uk>
2022-02-23 22:48:00 +0100

    Ensure that handlers close the input stream after doing their work.

--- core-vm/java/wonka/vm/CLJarHandler.java
+++ core-vm/java/wonka/vm/CLJarHandler.java
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
+* Copyright (c) 2022 by KIFFER Ltd. All rights reserved.                  *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -9,30 +9,27 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
+* 3. Neither the name of KIFFER Ltd nor the names of other contributors   *
+*    may be used to endorse or promote products derived from this         *
+*    software without specific prior written permission.                  *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
+* IN NO EVENT SHALL KIFFER LTD OR OTHER CONTRIBUTORS BE LIABLE FOR ANY    *
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      *
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE       *
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS           *
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER    *
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR         *
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF  *
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                              *
 **************************************************************************/
-
-/*
-** $Id: CLJarHandler.java,v 1.2 2006/05/16 08:24:41 cvs Exp $
-*/
 
 package wonka.vm;
 
 import java.io.InputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.jar.*;
 
@@ -53,24 +50,22 @@ public class CLJarHandler extends ClassLoaderURLHandler {
     }
   }
 
-  public byte[] getByteArray(String resource){
+  public byte[] getByteArray(String resource) throws IOException {
+    InputStream in = null;
     try {
       java.util.zip.ZipEntry ze = jf.getEntry(resource);
       if(ze != null){
-        InputStream in = jf.getInputStream(ze);
+        in = jf.getInputStream(ze);
         int len = in.available();
         byte[] bytes = new byte[len];
-
-        /**
-        ** since these bytes come from a ByteArrayInputStream we can thrust the read
-        ** to get all bytes in one run!
-        */
-
         in.read(bytes,0,len);
         return bytes;
       }
-    } catch(Exception e){
-      e.printStackTrace();
+    } catch(Exception e) {
+    } finally {
+      if (in != null) {
+        in.close();
+      }
     }
 
     return null;
--- core-vm/java/wonka/vm/CLUrlHandler.java
+++ core-vm/java/wonka/vm/CLUrlHandler.java
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Copyright (c) 2001 by Punch Telematix. All rights reserved.             *
+* Copyright (c) 2022 by KIFFER Ltd. All rights reserved.                  *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -9,26 +9,22 @@
 * 2. Redistributions in binary form must reproduce the above copyright    *
 *    notice, this list of conditions and the following disclaimer in the  *
 *    documentation and/or other materials provided with the distribution. *
-* 3. Neither the name of Punch Telematix nor the names of                 *
-*    other contributors may be used to endorse or promote products        *
-*    derived from this software without specific prior written permission.*
+* 3. Neither the name of KIFFER Ltd nor the names of other contributors   *
+*    may be used to endorse or promote products derived from this         *
+*    software without specific prior written permission.                  *
 *                                                                         *
 * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED          *
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF    *
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    *
-* IN NO EVENT SHALL PUNCH TELEMATIX OR OTHER CONTRIBUTORS BE LIABLE       *
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR            *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF    *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR         *
-* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,   *
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE    *
-* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  *
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           *
+* IN NO EVENT SHALL KIFFER LTD OR OTHER CONTRIBUTORS BE LIABLE FOR ANY    *
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      *
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE       *
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS           *
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER    *
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR         *
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF  *
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                              *
 **************************************************************************/
-
-/*
-** $Id: CLUrlHandler.java,v 1.2 2006/05/16 08:24:41 cvs Exp $
-*/
 
 package wonka.vm;
 
@@ -51,10 +47,11 @@ public class CLUrlHandler extends ClassLoaderURLHandler {
     }
   }
 
-  public byte[] getByteArray(String resource){
+  public byte[] getByteArray(String resource) throws IOException {
+    InputStream in = null;
     try {
       URL res = new URL(url, resource);
-      InputStream in = res.openStream();
+      in = res.openStream();
       byte[] bytes = new byte[1024];
       ByteArrayOutputStream out = new ByteArrayOutputStream(2048);
       int j = in.read(bytes,0,1024);
@@ -63,7 +60,12 @@ public class CLUrlHandler extends ClassLoaderURLHandler {
         j = in.read(bytes,0,1024);
       }
       return out.toByteArray();
-    } catch(Exception e){}
+    } catch(Exception e) {
+    } finally {
+      if (in != null) {
+        in.close();
+      }
+    }
 
     return null;
   }
