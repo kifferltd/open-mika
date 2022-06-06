494d7e701a369c0ba340fe36813be989708f7eb7
Chris Gray <chris.gray@imsystech.com>
2022-06-06 11:37:28 +0200

    Improvements to DNS name resoluton.

--- vm-cmp/network/native/hal/hostos/freertos/src/InetAddress.c
+++ vm-cmp/network/native/hal/hostos/freertos/src/InetAddress.c
@@ -26,8 +26,6 @@
 * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                              *
 **************************************************************************/
 
-// TODO - rewrite to work without JNI
-
 #include <network.h>
 #include <errno.h>
 
@@ -36,82 +34,13 @@
 #include "exception.h"
 #include "fields.h"
 #include "wstrings.h"
-#ifdef FREERTOS
 #include "FreeRTOS_IP.h"
 #define w_ntohl FreeRTOS_ntohl
-#else
-#define w_ntohl ntohl
-#endif
 
-/*
-** CG HACK to ensure we can always look up own name ...
-*/
-static char ownhostname[256];
 
+// CG I don't think this is needed on FreeRTOS
 w_boolean InetAddress_static_lookupName(w_thread thread, w_instance thisClass, w_instance InetAddress) {
-
-  w_boolean result = FALSE;
-#if NETWORKING == native
-  w_instance addrCache;
-  w_string name;
-// TODO make a w_hostent
-// struct hostent * host = NULL;
-  char * hostname;
-  int af = 0;
-
-  if (InetAddress == NULL) {
-    return FALSE;
-  }
-
-  addrCache = getReferenceField(InetAddress, F_InetAddress_ipAddressString);
-
-  if(addrCache == NULL) {
-    w_dump("InetAddress_lookupName: Avoiding Segfault\n");
-    return FALSE;
-  }
-
-  name = String2string(addrCache);
-
-  if(name == NULL) {
-    w_dump("w_string of String %p is NULL\n",addrCache);
-  }
-
-  hostname = allocMem(string_length(name) + 1);
-
-  if (!hostname) {
-    return FALSE;
-  }
-
-  x_snprintf(hostname, string_length(name) + 1, "%w", name);
-/* TODO re-write me!
-#ifdef AF_INET6
-  if(instance2clazz(InetAddress) == clazzInet6Address) {
-    af = AF_INET6;
-  }
-  else {
-    af = AF_INET;
-  }
-#else
-  af = AF_INET;
-#endif
-  host = w_gethostbyname(hostname);
-  if (host) {
-    woempa(7, "canonical name = '%s'\n", host->h_name);
-    w_string hname_string = cstring2String(host->h_name, strlen(host->h_name));
-    setReferenceField(InetAddress, newStringInstance(hname_string), F_InetAddress_hostName);
-    deregisterString(hname_string);
-    result = TRUE;
-  }
-  else {
-    woempa(7, "DNS lookup for '%s' failed! (Shouldn't we throw an exception?)\n", hostname);
-    // The exception should include hstrerror(h_errno)
-  }
-*/
-  releaseMem(hostname);
-#endif
-
-  return result;
-
+  return TRUE;
 }
 
 /**
@@ -119,63 +48,43 @@ w_boolean InetAddress_static_lookupName(w_thread thread, w_instance thisClass, w
 */
 
 void InetAddress_createInetAddress (w_thread thread, w_instance InetAddress, w_instance Name) {
+printf("Calling InetAddress_createInetAddress(%p, %p, %p)\n", thread, InetAddress, Name);
 
-#if NETWORKING == native
   long ipnumber;
   w_string name = String2string(Name);
-  w_string hname = NULL;
   char * hostname = allocMem(string_length(name) + 1);
-// TODO make a w_hostent
-//  struct hostent * host = NULL;
-  int af = 0;
+  w_word host;
 
   if (!hostname) {
     return;
   }
 
-/* TODO : re-write me
   x_snprintf(hostname, string_length(name) + 1, "%w", name);
-#ifdef AF_INET6
-  if(instance2clazz(InetAddress) == clazzInet6Address) {
-    af = AF_INET6;
+  // Check for special case where we are creating an InetAddress for our own IP interface
+  char ownhostname[32];
+  w_gethostname(ownhostname, 32);
+  if (strcmp(hostname, ownhostname) == 0) {
+    w_printf("looking up our own hostname %s\n", hostname);
+    host = FreeRTOS_IPAddress;
   }
   else {
-    af = AF_INET;
+    // Assuming that only IPv4 is supported
+    w_printf("looking up %s\n", hostname);
+    host = w_gethostbyname(hostname);
+    w_printf("result = %08x\n", host);
   }
-#else
-  af = AF_INET;
-#endif
-  //w_printf("looking up %s (%d)\n", hostname, af);
-  host = w_gethostbyname2(hostname, af);
-  //w_printf("result = %p\n", host);
 
   if (! host) {
     woempa(9, "unable to find DNS name for '%s' due to %s", hostname,strerror(errno));
-#ifdef UNC20
-    // No hstrerror()???
-    throwException(thread, clazzUnknownHostException, "unable to find DNS name for '%s'", hostname);
-#else
     // The exception should include hstrerror(h_errno)
     throwException(thread, clazzUnknownHostException, "unable to find DNS name for '%s'", hostname);
-#endif
     releaseMem(hostname);
     return;
   }
 
-  woempa(1, "displaying DNS information... name = %s (%p)\n", host->h_name, hostname);
-  woempa(1, "displaying DNS information... length = %d (%p)\n", host->h_length, hostname);
-*/
+  releaseMem(hostname);
 
-  /*
-  ** Creating and copying the hostname to private 'hostName' field of InetAddress Object
-  ** TODO : re-write me
-
-  //w_printf("canonical name = %s (%d)\n", hname, host->h_addrtype);
-  w_string hname_string = cstring2String(host->h_name, strlen(host->h_name));
-  setReferenceField(InetAddress, getStringInstance(hname_string), F_InetAddress_hostName);
-  deregisterString(hname_string);
-  //setReferenceField(InetAddress, Name, F_InetAddress_hostName);
-  */
+  setReferenceField(InetAddress, Name, F_InetAddress_hostName);
 
   /*
   ** Copying the 'type' into the 'type' private field of InetAddress Object
@@ -183,57 +92,39 @@ void InetAddress_createInetAddress (w_thread thread, w_instance InetAddress, w_i
   ** IPv4 	-> type = 2
   */
 
-/* TODO : re-write me
-  switch (host->h_addrtype) {
-  case (AF_LOCAL) :
+  // HACK HACK HACK
+  if ((host & 0xff000000) == 0x7f000000) {
     setIntegerField(InetAddress, F_InetAddress_family, 1);
-    break;
-
-  case (AF_INET) :
-    setIntegerField(InetAddress, F_InetAddress_family, 2);
-    break;
-
-  case (AF_INET6) :
-    setIntegerField(InetAddress, F_InetAddress_family, 6);
   }
-*/
+  else {
+    setIntegerField(InetAddress, F_InetAddress_family, 2);
+  }
 
   /*
   ** 1.Retrieving the dotted decimal representation of the ip number.
   ** 2.Copying the octet stuff into the 'address' private field of InetAddress Object
   */
 
-/* TODO : re-write me
-  ipnumber = *((w_word*)host->h_addr_list[0]);
-*/
-
-  releaseMem(hostname);
-  setIntegerField(InetAddress, F_InetAddress_address, w_ntohl(ipnumber));
+  setIntegerField(InetAddress, F_InetAddress_address, w_ntohl(host));
   woempa(7, "IP address is %d.%d.%d.%d\n", ((char*)&ipnumber)[0], ((char*)&ipnumber)[1], ((char*)&ipnumber)[2], ((char*)&ipnumber)[3]);
-#endif
+printf("InetAddress_createInetAddress(%p, %p, %p) -> %08x\n", thread, InetAddress, Name, host);
 }
 
 w_instance InetAddress_getLocalName(w_thread thread, w_instance theClass) {
-  w_clazz clazz = Class2clazz(theClass);
-  w_instance theName;
+printf("Calling InetAddress_getLocalName(%p, %p)\n", thread, theClass);
+  char ownhostname[32];
   w_string ownhostname_string;
-  w_boolean unsafe = enterUnsafeRegion(thread);
 
-  theName = allocInstance(thread, clazz);
-
-  if (!unsafe) {
-    enterSafeRegion(thread);
-  }
-
-  if (w_gethostname(ownhostname, 255) == 0) {
-    woempa(7, "Own host name is '%s'\n", ownhostname);
+  if (w_gethostname(ownhostname, 32) == 0) {
     ownhostname_string = cstring2String(ownhostname, strlen(ownhostname));
+    woempa(7, "Own host name is '%w'\n", ownhostname_string);
   }
   else {
     ownhostname_string = cstring2String("IM4000", 6);
   }
   
-  setReferenceField(theName, getStringInstance(ownhostname_string), F_InetAddress_hostName);
+printf("InetAddress_getLocalName(%p, %p) returned %w\n", thread, theClass, ownhostname_string);
+  w_instance theName = getStringInstance(ownhostname_string);
   deregisterString(ownhostname_string);
 
   return theName;
