1a53a485700e107da0ca4b50db661e055c51b760
Rickard <rickard.helmfrid@imsystech.com>
2023-12-06 10:50:39 +0100

    Create DefinetlyNotTakenClassName to initialize

--- Configuration/platform/im4000.mk
+++ Configuration/platform/im4000.mk
@@ -105,4 +105,3 @@ CFLAGS += -DJAVA_THREAD_YIELD_IS_FREERTOS_DELAY -DUSE_OBJECT_HASHTABLE
 
 # Enable this line if you want to run bytecode tests instead of an application
 # export BYTECODETEST = true
-
