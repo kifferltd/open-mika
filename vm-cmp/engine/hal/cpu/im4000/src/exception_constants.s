a904711c6c09434b0888d6074dbdbfc4f4f23389
Chris Gray <chris.gray@kiffer.ltd.uk>
2024-06-05 10:11:46 +0200

    Remove proprietary engine.

--- vm-cmp/engine/hal/cpu/im4000/src/exception_constants.s
+++
@@ -1,8 +0,0 @@
-
-;===========================================================
-; Microcoded Exception codes
-;
-EC_NULLPOINTEREXCEPTION = 0x11
-EC_ARRAYINDEXOUTOFBOUNDSEXCEPTION = 0x12
-EC_ARITHMETICEXCEPTION = 0X13
-EC_STACKOVERFLOWERROR = 0xFF
