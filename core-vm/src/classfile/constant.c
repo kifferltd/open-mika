6ebe96df0e34441501fd325f2f6a8ab2ca78e570
Chris Gray <chris.gray@kiffer.ltd.uk>
2024-09-03 10:50:11 +0200

    Bugfix: logic error re. throwing exception in reallyResolveMethodConstant.

--- core-vm/src/classfile/constant.c
+++ core-vm/src/classfile/constant.c
@@ -1,6 +1,6 @@
 /**************************************************************************
-* Copyright (c) 2008, 2009, 2010, 2015, 2023 by Chris Gray, KIFFER Ltd.   *
-* All rights reserved.                                                    *
+* Copyright (c) 2008, 2009, 2010, 2015, 2023, 2024 by Chris Gray,         *
+* KIFFER Ltd.  All rights reserved.                                       *
 *                                                                         *
 * Redistribution and use in source and binary forms, with or without      *
 * modification, are permitted provided that the following conditions      *
@@ -921,7 +921,7 @@ static void reallyResolveMethodConstant(w_clazz clazz, w_ConstantType *c, w_Cons
     }
 
 
-    if (!method && thread && !exceptionThrown(thread)) {
+    if (!method && (!thread || !exceptionThrown(thread))) {
         throwException(thread, clazzNoSuchMethodError, "%k: no method %w with signature %w found in %K or its superclasses", clazz, name, desc_string, search_clazz);
     }
 
